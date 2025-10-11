dbff1b9b239ae6932aba1946050ec8cede57b723
kaitoozawa <kaitozaw@gmail.com>
2025-10-11 14:30:47 +1000

    change sleep

--- orchestrators/orchestrator.py
+++ orchestrators/orchestrator.py
@@ -151,7 +151,7 @@ def main():
 
         # --- sleep ---
         if strategy == "boost":
-            busy_sleep = 3
+            busy_sleep = 10
             time.sleep(busy_sleep)
         else:
             base = 120
